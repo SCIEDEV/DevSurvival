790da38bf14b0680a7fd0d300129caff391a22cc
Jingcheng Yang <JingchengYang4@icloud.com>
2023-10-13 02:07:47 +0800

    Session 5

--- Assets/Scenes/Enemy.cs
+++ Assets/Scenes/Enemy.cs
@@ -1,21 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
     public float health = 20f;
+
+    public float damage = 10f;
+
+    private NavMeshAgent agent;
+    private Transform player;
+
+    public GameObject explosion;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(player.position);
+    }
 
     public void TakeDamage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            Instantiate(explosion, transform.position, explosion.transform.rotation);
             Destroy(gameObject);
         }
         else
         {
             transform.localScale *= 0.7f + 0.3f * (health / 20f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.TakeDamage(damage);
+            Instantiate(explosion, transform.position, explosion.transform.rotation);
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 4.0f;
@@ -12,32 +13,45 @@ public class Player : MonoBehaviour
 
     public float projectileVelocity = 30f;
 
+    private CharacterController controller;
+
+    public RectTransform healthBar;
+
+    public float playerMaxHealth = 100;
+    private float playerHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+
+        playerHealth = playerMaxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 moveDisplacement = Vector3.zero;
+        
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, 0, moveSpeed) * Time.deltaTime;
+            moveDisplacement += new Vector3(0, 0, moveSpeed);// * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.position -= new Vector3(0, 0, moveSpeed) * Time.deltaTime;
+            moveDisplacement -= new Vector3(0, 0, moveSpeed);// * Time.deltaTime;
         }
         
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(moveSpeed, 0, 0) * Time.deltaTime;
+            moveDisplacement += new Vector3(moveSpeed, 0, 0);// * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= new Vector3(moveSpeed, 0, 0) * Time.deltaTime;
+            moveDisplacement -= new Vector3(moveSpeed, 0, 0);// * Time.deltaTime;
         }
+
+        controller.SimpleMove(moveDisplacement);
 
         if (Input.GetMouseButton(0))
         {
@@ -46,6 +60,7 @@ public class Player : MonoBehaviour
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hitOutput;
+                
                 var hit = Physics.Raycast(ray, out hitOutput, 100);
                 if (hit)
                 {
@@ -62,5 +77,22 @@ public class Player : MonoBehaviour
             }
 
         }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
+
+        //calculate percentage (0-1) of player health
+        float percentage = playerHealth / playerMaxHealth;
+
+        //get the MAX width of the healthbar (through finding the width of its parent)
+        float parentWidth = ((RectTransform)healthBar.parent.transform).rect.width;
+
+        //calculate the inverse of the bar width as a distance from the right, thus 1- percentage
+        float barWidth = (1 - percentage) * parentWidth;
+
+        //apply the offset (the RIGHT variable on the rect inspector)
+        healthBar.offsetMax = new Vector2(-barWidth ,0);
     }
 }
